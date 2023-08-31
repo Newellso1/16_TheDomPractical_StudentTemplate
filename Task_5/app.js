5b5d8498d123feaab319f46e489653dd3864c465
Sam <samnewell91@gmail.com>
2023-08-31 17:21:04 +0100

    update search query

--- Task_5/app.js
+++ Task_5/app.js
@@ -1,27 +1,14 @@
 const animalRadios = document.getElementsByName('animalType');
 const images = document.getElementsByClassName('imageFilter');
 const search = document.getElementById('search');
-
+const message = document.getElementById('searchMessage');
 for (const animalRadio of animalRadios) {
     animalRadio.addEventListener('change', filterAnimals)
 };
 
-
-
-
-function filterAnimals(e) {
-    const selectedAnimal = e.target.value;
-    for (const image of images) {
-        if (selectedAnimal === 'all' || selectedAnimal === image.getAttribute('animal')) {
-            image.classList.remove('hidden');
-        } else {
-            image.classList.add('hidden');
-        }
-    }
-};
-
 search.addEventListener('keyup', function () {
     const searchValue = search.value.toLowerCase();
+    
     for (const image of images) {
         const altText = image.getAttribute('alt').toLowerCase();
         if(altText.includes(searchValue)){
@@ -31,6 +18,19 @@ search.addEventListener('keyup', function () {
         }
     }
 })
+
+function filterAnimals(e) {
+    const selectedAnimal = e.target.value;
+    for (const image of images) {
+        if (selectedAnimal === 'all' || selectedAnimal === image.getAttribute('animal')) {
+            image.classList.remove('hidden');
+            message.innerHTML = `Showing ${selectedAnimal}`;
+        } else {
+            image.classList.add('hidden');
+        }
+    }
+};
+
 
 search.addEventListener('keydown', function(e) {
     if(e.key === 'Enter') {
